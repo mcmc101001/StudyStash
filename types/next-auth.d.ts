5d0fba827787af82f3f577872182978179f1aa96
mcmc101001 <e0360573@u.nus.edu>
2023-04-04 12:06:19 +0800

    added types folder to remove typescript nextauth errors

---
+++ types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth, { DefaultSession } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+type userID = string;
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      id: userID
+    } & DefaultSession["user"]
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    id?: userID
+  } 
+}
