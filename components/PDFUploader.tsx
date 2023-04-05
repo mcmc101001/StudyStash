19c6aec0c9a93371c99e187831d3d8c264ba6ce3
mcmc101001 <e0360573@u.nus.edu>
2023-04-05 14:26:26 +0800

    added safe file name parse

--- components/PDFUploader.tsx
+++ components/PDFUploader.tsx
@@ -1,9 +1,22 @@
+/*
+* Please note: TO CHANGE
+* Use unique file id as key to upload file
+* Take note
+*/
+
+
 "use client";
 
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import axios from "axios";
 import { useRef } from "react";
+
+const MAX_FILE_SIZE = 10485760; // 10Mb
+
+const cleanup = function(str: string) {
+    return str.replace(/[^a-zA-Z0-9_.{}\- ]/g,'-');
+}
 
 export default function ResumeUploader() {
 
@@ -18,6 +31,10 @@ export default function ResumeUploader() {
                 toast.error("Please upload a PDF file");
                 // Set submit to disabled
                 setIsDisabled(true);
+            } else if (e.currentTarget.files[0].size > MAX_FILE_SIZE) {
+                toast.error("Max file size: 10Mb");
+                // Set submit to disabled
+                setIsDisabled(true);
             } else {
               setIsDisabled(false);
             }
@@ -30,25 +47,29 @@ export default function ResumeUploader() {
             toast.error("Please upload a PDF file");
             return;
         } else {
-            const safeFileName = file.name.replace(/[\[\]\(\)']+/g,'');
-            console.log(safeFileName);
-
-            let { data } = await axios.post('/api/addPDF', {
-                name: safeFileName,
-                type: file.type,
-            });
-
-            const url = data.url;
-
-            await axios.put(url, file, {
-              headers: {
-                "Content-Type": "application/pdf",
-                "Content-Disposition": "inline",
-                "Access-Control-Allow-Origin": "*",
-              },
-            })
-            
-            toast.success('PDF uploaded successfully');
+            const safeFileName = cleanup(file.name);
+            try {
+                let { data } = await axios.post('/api/addPDF', {
+                    name: safeFileName,
+                    type: file.type,
+                });
+    
+                const url = data.url;
+    
+                await axios.put(url, file, {
+                  headers: {
+                    "Content-Type": "application/pdf",
+                    "Content-Disposition": "inline",
+                    "Access-Control-Allow-Origin": "*",
+                  },
+                })
+                
+                toast.success('PDF uploaded successfully');
+                
+            } catch (error) {
+                console.log(error);
+                toast.error('Error uploading PDF.');
+            }
             if (inputRef.current) {
                 inputRef.current.value = '';
             }
