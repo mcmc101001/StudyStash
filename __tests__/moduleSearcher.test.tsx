5beb3eb7b3f938ea1deb6a3a0f265b9c1c22c7ce
mcmc101001 <e0360573@u.nus.edu>
2023-04-23 11:00:52 +0800

    added navbar tests skeleton

--- __tests__/moduleSearcher.test.tsx
+++ __tests__/moduleSearcher.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import ModuleSearcher from "@/components/ModuleSearcher";
 import { useSelectedLayoutSegments } from "next/navigation";
---
+++ __tests__/navBar.test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import NavBar from "@/components/NavBar";
+import { getCurrentUser } from "@/lib/session";
+import { useSelectedLayoutSegment } from "next/navigation";
+import "@testing-library/jest-dom/extend-expect";
+import { navOptions } from "@/components/NavBar";
+
+const mock_user = {
+  name: "test",
+  email: "test@gmail.com",
+  image: "test.png",
+};
+
+const mock_segment = "database";
+
+jest.mock("next/navigation", () => ({
+  useSelectedLayoutSegment: jest.fn(),
+}));
+
+jest.mock("../lib/session", () => ({
+  getCurrentUser: jest.fn(),
+}));
+
+/*
+ * TEST DOES NOT WORK DUE TO NESTED SERVER COMPONENTS
+ * FIX NOT FOUND
+ */
+
+describe("Module Searcher", () => {
+  it("should return true", async () => {
+    expect(true).toBe(true);
+  });
+  // it("should render navbar with correct number of nav options", async () => {
+  //   (useSelectedLayoutSegment as jest.Mock).mockReturnValue(mock_segment);
+  //   (getCurrentUser as jest.Mock).mockReturnValue(mock_user);
+  //   render(<NavBar />);
+  //   await waitFor(() => {
+  //     const navBar = screen.getByRole("navigation");
+  //     expect(navBar.childElementCount).toBe(navOptions.length);
+  //   });
+  // });
+
+  // navOptions.map((option) => {
+  //   it(`should render ${option.name} nav option with correct border`, async () => {
+  //     (useSelectedLayoutSegment as jest.Mock).mockReturnValue(mock_segment);
+  //     (getCurrentUser as jest.Mock).mockReturnValue(mock_user);
+  //     render(<NavBar />);
+  //     const navOption = screen.getByRole("link", { name: option.name });
+  //     expect(navOption).toBeInTheDocument();
+  //     if (option.name === "database") {
+  //       expect(navOption.className).toContain("border");
+  //     }
+  //   });
+  // });
+});
