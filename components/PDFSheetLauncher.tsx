b1efe95ffc9abddae0e46d3c7dcb7ec6373f19a2
mcmc101001 <e0360573@u.nus.edu>
2023-04-24 20:05:45 +0800

    reverted nextjs version to 13.2.4 due to shallow routing bug

--- components/PDFSheetLauncher.tsx
+++ components/PDFSheetLauncher.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/Sheet";
 import Link from "next/link";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { RouteHandlerManager } from "next/dist/server/future/route-handler-managers/route-handler-manager";
 
 interface PDFSheetLauncherProps {
   children: React.ReactNode;
@@ -17,14 +18,24 @@ interface PDFSheetLauncherProps {
 }
 
 const PDFSheetLauncher: FC<PDFSheetLauncherProps> = ({ children, id }) => {
+  const router = useRouter();
   const pathnames = usePathname();
   const searchParams = useSearchParams()!;
   const searchParamsID = searchParams.get("id");
+  const url = `${pathnames}/?id=${id}`;
 
   return (
-    <Sheet open={id === searchParamsID}>
-      <Link href={`${pathnames}/?id=${id}`}>{children}</Link>
-      <SheetContent size={"xl"}>
+    <Sheet
+      onOpenChange={() => {
+        router.push(url);
+      }}
+    >
+      <SheetTrigger>{children}</SheetTrigger>
+      <SheetContent
+        size={"xl"}
+        onEscapeKeyDown={router.back}
+        // onPointerDownOutside={router.back}
+      >
         <SheetHeader>
           <SheetTitle>Are you sure absolutely sure?</SheetTitle>
           <SheetDescription>
