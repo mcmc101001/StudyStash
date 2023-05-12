df66d3fac3b6d4ab70b0a1ec04057793339a30d7
mc101001 <tohmingchun101001@gmail.com>
2023-05-12 22:58:50 +0800

    added skeleton of tab based navigation with search params in profile page

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,14 +1,17 @@
-import UserResources from "@/components/UserResources";
+import UserResourcesSection from "@/components/UserResourcesSection";
 import { prisma } from "@/lib/prisma";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import ProfileEditDialog from "@/components/ProfileEditDialog";
+import { ResourceFiltersSorts } from "@/lib/content";
 
 export default async function ProfilePageUser({
   params,
+  searchParams,
 }: {
   params: { userId: string };
+  searchParams: ResourceFiltersSorts;
 }) {
   const currentUser = await getCurrentUser();
   const isProfile = params.userId === currentUser?.id;
@@ -24,37 +27,46 @@ export default async function ProfilePageUser({
   }
 
   return (
-    <div className="m-20 text-slate-800 dark:text-slate-200">
-      <div className="flex justify-between">
-        <div>
-          <Image
-            src={profileUser.image!}
-            width={75}
-            height={75}
-            alt="Profile pic"
-          ></Image>
-          <h1 className="text-xl font-bold">{profileUser.name}</h1>
-          <div className="my-2">
-            <h2 className="text-lg font-bold">Bio</h2>
-            <p>
-              {profileUser.bio ||
-                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
-            </p>
+    <div className="m-20 flex gap-x-10 text-slate-800 dark:text-slate-200">
+      <div className="w-1/2">
+        <div className="flex justify-between">
+          <div>
+            <Image
+              src={profileUser.image!}
+              width={75}
+              height={75}
+              alt="Profile pic"
+            ></Image>
+            <h1 className="text-xl font-bold">{profileUser.name}</h1>
+            <div className="my-2">
+              <h2 className="text-lg font-bold">Bio</h2>
+              <p>
+                {profileUser.bio ||
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
+              </p>
+            </div>
+            <h2 className="my-2 text-lg font-semibold">My resources</h2>
           </div>
-          <h2 className="my-2 text-lg font-semibold">My resources</h2>
+          <div className="">
+            {isProfile && (
+              <ProfileEditDialog
+                userId={profileUser.id}
+                username={profileUser.name ?? "name not found"}
+                bio={profileUser.bio}
+              />
+            )}
+          </div>
         </div>
-        <div className="">
-          {isProfile && (
-            <ProfileEditDialog
-              userId={profileUser.id}
-              username={profileUser.name ?? "name not found"}
-              bio={profileUser.bio}
-            />
-          )}
-        </div>
+        {/* @ts-expect-error Server Component */}
+        <UserResourcesSection
+          filterCategory={searchParams.filterCategory}
+          isProfile={isProfile}
+          profileUserId={profileUser.id}
+        />
       </div>
-      {/* @ts-expect-error Server Component */}
-      <UserResources />
+      <div className="flex w-1/2 items-center justify-center bg-slate-600 text-center text-xl">
+        points or achievements
+      </div>
     </div>
   );
 }
