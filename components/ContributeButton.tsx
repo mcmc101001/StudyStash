82d0340e0f6b67ac35a5e65d5762c4954a28d736
marquestye <marquestyes@gmail.com>
2023-05-16 18:59:52 +0800

    forgot to add before committing :)

---
+++ components/ContributeButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ResourceTypeURL } from "@/lib/content";
+import Link from "next/link";
+
+interface ContributeButtonProps {
+  category: ResourceTypeURL;
+  moduleCode: string;
+  queryParams: string | undefined;
+}
+
+export default function ContributeButton({
+  category,
+  moduleCode,
+  queryParams,
+}: ContributeButtonProps) {
+  return (
+    <Link
+      href={
+        "/addPDF/" +
+        category +
+        "/?filterModuleCode=" +
+        moduleCode +
+        "&" +
+        queryParams
+      }
+      className="absolute right-16 top-12 rounded-md border border-slate-800 p-2 font-semibold dark:border-slate-200"
+    >
+      Contribute
+    </Link>
+  );
+}
