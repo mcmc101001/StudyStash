5fa1eab851f4712702a5a30828e53c38851e7e5c
mc101001 <tohmingchun101001@gmail.com>
2023-06-04 11:15:00 +0800

    fixed invisble a tage being rendered when no module is searched in database

--- components/ModuleList.tsx
+++ components/ModuleList.tsx
@@ -22,23 +22,24 @@ export default function ModuleList(props: ModuleListProps) {
 
   return (
     <ul role="list" className="flex flex-1 flex-col gap-y-7">
-      {props.moduleCodes.map((mod) => {
-        return (
-          <li key={mod}>
-            <Link
-              className={
-                "p-2 text-slate-800 transition-colors dark:text-slate-200 " +
-                (mod === props.selectedModule
-                  ? "border border-slate-800 dark:border-slate-200 "
-                  : "hover:text-slate-600 dark:hover:text-slate-400 ")
-              }
-              href={`/database/${mod}/${linkExtension}`}
-            >
-              {mod}
-            </Link>
-          </li>
-        );
-      })}
+      {props.moduleCodes.length !== 0 &&
+        props.moduleCodes.map((mod) => {
+          return (
+            <li key={mod}>
+              <Link
+                className={
+                  "p-2 text-slate-800 transition-colors dark:text-slate-200 " +
+                  (mod === props.selectedModule
+                    ? "border border-slate-800 dark:border-slate-200 "
+                    : "hover:text-slate-600 dark:hover:text-slate-400 ")
+                }
+                href={`/database/${mod}/${linkExtension}`}
+              >
+                {mod}
+              </Link>
+            </li>
+          );
+        })}
     </ul>
   );
 }
