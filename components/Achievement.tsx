29aa291aba27b3e3cd26f8677f263cfbed9733af
mc101001 <tohmingchun101001@gmail.com>
2023-06-15 22:21:57 +0800

    added statistics

---
+++ components/Achievement.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Icon, Icons } from "./Icons";
+
+interface AchievementProps {
+  description: string;
+  value: number;
+  icon: Icon;
+}
+
+export function Achievement({ description, value, icon }: AchievementProps) {
+  const Icon = Icons[icon];
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-950 p-5 text-2xl font-semibold">
+        <Icon className="h-full w-full" />
+      </div>
+      <div className="mt-2 flex items-center justify-center text-3xl font-semibold">
+        {value}
+      </div>
+      <div className="font-light text-slate-400">{description}</div>
+    </div>
+  );
+}
