387520e25a3e40c9a62e9ed9dd61fb5d7d40d9f2
mc101001 <tohmingchun101001@gmail.com>
2023-06-17 21:05:28 +0800

    amended dashboard default query params

--- components/nav/NavOptions.tsx
+++ components/nav/NavOptions.tsx
@@ -47,7 +47,15 @@ export default function NavOptions({
               }
             >
               {href === "/database" || userId ? (
-                <Link aria-label={href} className="p-2" href={href === "/database" ? (href + "?filterStatus=Todo&filterCategory=cheatsheets") : href}>
+                <Link
+                  aria-label={href}
+                  className="p-2"
+                  href={
+                    href === "/dashboard"
+                      ? href + "?filterStatus=Todo&filterCategory=cheatsheets"
+                      : href
+                  }
+                >
                   <Icon className="h-7 w-7" />
                 </Link>
               ) : (
