56d456becfb3acf74eacae6c89c68c8321935db8
marquestye <marquestyes@gmail.com>
2023-06-22 01:08:46 +0800

    code to reset visited database, should delete after run at least once

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,12 +27,12 @@ export default async function DashboardPage({
   });
 
   // IMPT: REMOVE THIS LATER
-  // await prisma.user.updateMany({
-  //   data: {
-  //     visitedData:
-  //       '{"visitedCheatsheets":[],"visitedPastPapers":[],"visitedNotes":[],"visitedSolutions":[]}',
-  //   },
-  // });
+  await prisma.user.updateMany({
+    data: {
+      visitedData:
+        '{"visitedCheatsheets":[],"visitedPastPapers":[],"visitedNotes":[],"visitedSolutions":[]}',
+    },
+  });
 
   let visitedDataObject: VisitedDataType | undefined = undefined;
   if (searchParams.filterStatus === "Visited") {
