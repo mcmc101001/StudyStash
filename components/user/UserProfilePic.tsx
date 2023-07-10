f777c152822831acdc108832561654b2d3eb900f
marquestye <marquestyes@gmail.com>
2023-07-10 14:05:20 +0800

    pfp links to profile page

--- components/user/UserProfilePic.tsx
+++ components/user/UserProfilePic.tsx
@@ -2,6 +2,7 @@ import { getCurrentUser } from "@/lib/session";
 import Image from "next/image";
 import LogoutButton from "@/components/nav/LogoutButton";
 import LoginButton from "@/components/nav/LoginButton";
+import Link from "next/link";
 
 async function UserProfilePic() {
   const user = await getCurrentUser();
@@ -9,15 +10,17 @@ async function UserProfilePic() {
     <div className="flex flex-col items-center justify-center gap-y-4">
       {user ? (
         <>
-          <Image
-            loading="lazy"
-            src={user.image!}
-            alt={user.name ?? "profile image"}
-            referrerPolicy="no-referrer"
-            width={60}
-            height={60}
-            className="rounded-xl"
-          />
+          <Link href={`/profile/${user.id}?filterCategory=cheatsheets`}>
+            <Image
+              loading="lazy"
+              src={user.image!}
+              alt={user.name ?? "profile image"}
+              referrerPolicy="no-referrer"
+              width={60}
+              height={60}
+              className="rounded-xl"
+            />
+          </Link>
           <LogoutButton />
         </>
       ) : (
